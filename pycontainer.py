23b6d09cff75bb739fe1ac31cf18c3db1005586c
imp <1013172219@qq.com>
2026-03-18 12:38:37 +0800

    v3.0

---
+++ pycontainer.py
@@ -0,0 +1,31 @@
+# # 3.1 list
+# list 的定义  用于储存一组数据 火车 
+game_list = ["GTA V", "Crysis", "CSGO", "Minecraft"]
+
+print(game_list)
+# list 查询数据
+
+game_list[0] 
+
+print(game_list[0])
+
+game_list[1] = "Witcher 3"
+
+print(game_list)
+
+del game_list[1]
+print(game_list)
+
+
+# 3.2 tuple
+
+
+# 3.3 set
+
+
+# 3.4 dictionary
+
+
+# 3.5 string
+
+# 3.6 boolean
